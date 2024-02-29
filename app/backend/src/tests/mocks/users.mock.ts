9c7948e10e60629558e5579be78730e1a2ea0302
analaurafr <laurafernandes@outlook.com>
2024-02-29 19:01:04 -0300

    testes

---
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -0,0 +1,24 @@
+const user =
+  {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  }
+
+  const login = {
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  }
+
+  const loginFalse = {
+    email: 'admin@admin.com',
+    password: '$2a$10$HDkFwOMKOI6PTza0F7.YRu1Bqsqb9hx7XkuV7QeYB5dRL4z9DI1M'
+  }
+
+  export {
+    user,
+    login,
+    loginFalse
+  }
